fb3345b1f50b3eea99980828545ca62981de9c06
mtomczak <iam@fixermark.com>
2011-09-11 09:11:34 -0400

    Removed unused (misdirecting) servant.rb file.

--- servant.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'belphanior/servant/servant'
-require 'sinatra'
-
-# An empty example; this is the simplest server that can be created.
-
-# Must be called to install and configure default routing for
-# '/', '/config', and '/protocol'
-servant_init
